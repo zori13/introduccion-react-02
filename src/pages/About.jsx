0248c84a0060284c41bc9c1e5f3981c90cedb7ea
zori13 <zori.gon@mail.com>
2023-12-01 13:18:34 +0100

    Ejercicio 3

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import Navegacion from '../components/Ejercicio3/Navegacion'
+export default function About(){
+    return (
+        <div>
+            <h1>Sobre Nosotros</h1>
+            <Navegacion/>
+        </div>
+    )
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import Navegacion from '../components/Ejercicio3/Navegacion'
+export default function Contact(){
+
+    return(
+        <div>
+            <h1>Formulario de Contacto</h1>
+            <Navegacion/>
+        </div>
+    )
+}
